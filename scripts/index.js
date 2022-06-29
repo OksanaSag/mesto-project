71c9d0a79d786b68b008f7d75072e6ae74a0d7cd
OksanaSag <sagitovaoksana31@gmail.com>
2022-06-29 15:14:39 +0700

    corrected code

--- scripts/index.js
+++ scripts/index.js
@@ -2,27 +2,27 @@
     const newPictureButton = document.querySelector('.profile__add-picture');
     const profileCloseButton = document.querySelector('#formClose');
     const newPictureCloseButton = document.querySelector('#formClosePicture');
-    const formEditProfile = document.querySelector('.form__position');
-    const newPictureForm = document.querySelector('#form-Position');
+    const formEditProfile = document.querySelector('#profileName');
+    const newPictureForm = document.querySelector('#profilePicture');
     const bigPictureClose = document.querySelector('#formCloseBigPicture');
     profileEditPopup.addEventListener('click', openPopup);
     newPictureButton.addEventListener('click', openPopup);
     profileCloseButton.addEventListener('click', closePopup);
     newPictureCloseButton.addEventListener('click', closePopup);
     bigPictureClose.addEventListener('click', closePopup);
-    profileEditPopup.openForm = document.querySelector('#formOpen'); 
-    newPictureButton.openForm = document.querySelector('#formOpenPicture'); 
-    profileCloseButton.closeForm = document.querySelector('#formOpen'); 
-    newPictureCloseButton.closeForm = document.querySelector('#formOpenPicture'); 
-    formEditProfile.closeForm = document.querySelector('#formOpen'); 
-    newPictureForm.closeForm = document.querySelector('#formOpenPicture'); 
-    bigPictureClose.closeForm = document.querySelector('#bigPicture'); 
+    profileEditPopup.formOpen = document.querySelector('#formOpen'); 
+    newPictureButton.formOpen = document.querySelector('#formOpenPicture'); 
+    profileCloseButton.formClose = document.querySelector('#formOpen'); 
+    newPictureCloseButton.formClose = document.querySelector('#formOpenPicture'); 
+    formEditProfile.formClose = document.querySelector('#formOpen'); 
+    newPictureForm.formClose = document.querySelector('#formOpenPicture'); 
+    bigPictureClose.formClose = document.querySelector('#bigPicture'); 
 
 function openPopup(evt) {
-    evt.currentTarget.openForm.classList.add('form_opened');
+    evt.currentTarget.formOpen.classList.add('form_opened');
 }
 function closePopup(evt) {
-    evt.currentTarget.closeForm.classList.remove('form_opened');
+    evt.currentTarget.formClose.classList.remove('form_opened');
 }
 
     const nameInput = document.querySelector('.form__line_box_name');
@@ -40,6 +40,7 @@ function editProfile(evt) {
     const popupBigPicture = document.querySelector('#bigPicture');
 function openPicture (evt) {
     popupBigPicture.querySelector('.form__image').src = evt.target.src;
+    popupBigPicture.querySelector('.form__image').alt = 'Изображениe';
     const pictureCaption = popupBigPicture.querySelector('.form__caption');
     pictureCaption.textContent = evt.target.parentNode.querySelector('.elements__caption').textContent;
     openPopup(evt);
@@ -64,9 +65,10 @@ function createCard(element){
     });
 
     elementImage.addEventListener('click', openPicture);
-    elementImage.openForm = document.querySelector('#bigPicture'); 
+    elementImage.formOpen = document.querySelector('#bigPicture'); 
     elelementsCaption.textContent = element.name;
     elementImage.src = element.link;
+    elementImage.alt = 'Изображениe';
     return clone;
 }
 
